fd02df61b9f3849fe1682c43d8acb36a95f602d0
Camille Salvador <camille.salvador@outlook.com>
2017-09-14 22:26:44 -0600

    test

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@ module.exports = {
   exportPathMap() {
     return {
       '/': { page: '/' },
-      '/corporate': { page: '/' },
-      '/fun': { page: '/'},
-      '/energetic': { page: '/' },
-      '/relaxed': { page: '/' }
+      
     };
   }
 };
